6af1ec1aee8d98c22a6553a6f502e5d6f4f24057
Juhapekka Piiroinen <juhapekka.piiroinen@csc.fi>
2019-09-04 10:25:51 +0300

    CSCETSIN-504: Fixed broken download implementation on frontend.

--- etsin_finder/frontend/js/components/dataset/data/tableItem.jsx
+++ etsin_finder/frontend/js/components/dataset/data/tableItem.jsx
@@ -15,7 +15,6 @@ import styled, { withTheme } from 'styled-components'
 import Translate from 'react-translate-component'
 import translate from 'counterpart'
 import PropTypes from 'prop-types'
-import axios from 'axios'
 
 import checkDataLang, { getDataLang } from '../../../utils/checkDataLang'
 import sizeParse from '../../../utils/sizeParse'
@@ -73,58 +72,32 @@ class TableItem extends Component {
   }
 
   downloadItem() {
-    this.setState({ downloadDisabled: true },
-      () => {
-        const url = `/api/dl?cr_id=${this.props.cr_id}${this.props.item.type === 'dir'
+    this.setState(
+      {
+        downloadDisabled: true
+      }, () => {
+        const downloadUrl = `/api/dl?cr_id=${this.props.cr_id}${this.props.item.type === 'dir'
           ? `&dir_id=${this.props.item.identifier}`
           : `&file_id=${this.props.item.identifier}`}`
-        axios.get(url)
-          .then((res) => {
-            console.groupCollapsed('%cDownload Response', 'color: blue;')
-            console.log('Response data:', res.data);
-            console.log('Response status:', res.status);
-            console.log('Response status text:', res.statusText);
-            console.log('Response headers:', res.headers);
-            console.log('Config:', res.config);
-            console.groupEnd()
-            this.setState({
-              downloadDisabled: false,
-              downloadFailed: false
-            })
-            const a = document.createElement('a')
-            a.href = url
-            a.setAttribute('download', this.props.item.name)
-            document.body.appendChild(a)
-            a.click()
-            // Cleanup
-            window.URL.revokeObjectURL(a.href);
-            document.body.removeChild(a);
+        function onStartedDownload(id) {
+          console.log(`Started downloading: ${id}`);
+          this.setState({
+            downloadDisabled: false,
+            downloadFailed: false
           })
-          .catch((err) => {
-            console.groupCollapsed('%cError in Download', 'color: red;')
-            if (err.response) {
-              // The request was made and the server responded with a status code
-              // that falls out of the range of 2xx
-              console.log(`Response status: %c${err.response.status}`, 'color: red;');
-              console.log('Response data:', err.response.data);
-              console.log('Response headers:', err.response.headers);
-            } else if (err.request) {
-              // The request was made but no response was received
-              // `err.request` is an instance of XMLHttpRequest in the browser and an instance of
-              // http.ClientRequest in node.js
-              console.log('Request: ');
-              console.log(err.request);
-            } else {
-              // Something happened in setting up the request that triggered an Error
-              console.log('Error', err.message);
-            }
-            console.log('Config:', err.config);
-            this.setState({
-              downloadDisabled: false,
-              downloadFailed: true
-            })
-            console.groupEnd()
+        }
+        function onFailed(error) {
+          console.error(`Download failed: ${error}`);
+          this.setState({
+            downloadDisabled: false,
+            downloadFailed: true
           })
+        }
+        var downloading = browser.downloads.download({
+          url : downloadUrl,
+          saveAs : true
+        });
+        downloading.then(onStartedDownload, onFailed);
       }
     )
   }
